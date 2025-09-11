631a09e2bbecc08ef8d9b20cb62b13826ecf4c82
ParmanGitijah <Parman.gitijah@chasacademy.se>
2025-09-11 17:37:06 +0200

    Renamed Controller

---
+++ Controllers/AdminBookingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResturantPG_MVC.Controllers
+{
+    public class AdminManageBookingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
